0c8f7b288b8f471e18911cdd22dd504bcc3a0c06
qqgameover <kasper35@live.no>
2021-11-13 00:18:38 +0100

    Initial commit

---
+++ SQLTest/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLTest
+{
+    class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public string MiddleName;
+    }
+}
---
+++ SQLTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace SQLTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestQuery().Wait();
+        }
+
+        public static async Task TestQuery()
+        {
+            var connectionStr =
+                "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=KasperTest;" +
+                "Integrated Security=True;Connect Timeout=30;Encrypt=False;" +
+                "TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            var conn = new SqlConnection(connectionStr);
+
+            var readAll = @"SELECT * from Person";
+            var spesificQuery = @"SELECT FirstName, LastName, Id from Person 
+                                WHERE LastName LIKE '%Teacher%'";
+
+            var addOne = @"INSERT INTO Person
+                            (FirstName, LastName, MilddeName)
+                            VALUES('Terje', 'Teacher', null)";
+            //await conn.ExecuteAsync(addOne);
+            var persons = await conn.QueryAsync<Person>(readAll);
+            persons.ToList().ForEach(x => Console.WriteLine($"Id={x.Id}, {x.FirstName} {x.LastName}"));
+            Console.WriteLine();
+            var teachers = await conn.QueryAsync(spesificQuery);
+            teachers.ToList().ForEach(x => Console.WriteLine($"Id={x.Id}, {x.FirstName} {x.LastName}"));
+        }
+    }
+}
