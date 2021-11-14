2996b3ac1a4efc42d3859c6936a76027c403289e
qqgameover <kasper35@live.no>
2021-11-14 01:01:17 +0100

    Update Person.cs

--- SQLTest/Person.cs
+++ SQLTest/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SQLTest
 {
-    class Person
+    public class Person
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
